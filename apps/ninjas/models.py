f9de6565f701b9c5d23396922ca5493a53957ee6
Elizabeth (Lizzie) Gutierrez <elizabeth.wk.gutierrez@gmail.com>
2017-10-08 11:11:34 -0700

    Models (#25)
    * Created ninja app, ninja model, and made sure we could create and save ninja.
    
    * Added validations to Ninja and tested that username and email are unique upon registration.
    
    * Cleaned up error return message(s).

---
+++ apps/ninjas/models.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User, UserManager
+
+class NinjaManager(UserManager):
+    def validate_registration(self, data):
+        emails = self.filter(email=data['email'])
+        usernames = self.filter(username=data['username'])
+        errors = {}
+        if len(emails) > 0:
+            errors['email'] = 'Email already exists.'
+        if len(usernames) > 0:
+            errors['username'] = 'Username already exists.'
+        if not errors:
+            new_ninja = self.create_user(
+                data['username'],
+                data['email'],
+                data['password'],
+            )
+            new_ninja.first_name = data['first_name']
+            new_ninja.last_name = data['last_name']
+            new_ninja.save()
+            return new_ninja
+        else:
+            return errors
+            
+
+
+class Ninja(User):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = NinjaManager()
+    
+    @property
+    def challenges_won(self):
+        return 0
+
+    @property
+    def challenges_lost(self):
+        return 0
---
+++ apps/ninjas/tests.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Ninja
+
+# Test that we can create and save user
+class UsersTest(TestCase):
+    def test_that_user_can_be_created_and_saved(self):
+        ninjas = Ninja.objects.create_user('bob', 'bobby@ross.com', 'password')
+        all_ninjas = Ninja.objects.all()
+        self.assertIn(ninjas, all_ninjas)
+
+    def test_that_username_is_unique(self):
+
+        bobby = Ninja.objects.validate_registration({
+            'username': 'bobby',
+            'email': 'bobby@ross.com',
+            'password': 'password',
+            'first_name': 'bob',
+            'last_name': 'ross'
+        })
+
+        jorge = Ninja.objects.validate_registration({
+            'username': 'bobby',
+            'email': 'rossy@ross.com',
+            'password': 'password',
+            'first_name': 'bob',
+            'last_name': 'ross'
+        })
+
+        self.assertEqual(Ninja.objects.count(), 1)
+
+    def test_that_email_is_unique(self):
+    
+        bobby = Ninja.objects.validate_registration({
+            'username': 'bobby',
+            'email': 'bobby@ross.com',
+            'password': 'password',
+            'first_name': 'bob',
+            'last_name': 'ross'
+        })
+
+        jorge = Ninja.objects.validate_registration({
+            'username': 'jorgy',
+            'email': 'bobby@ross.com',
+            'password': 'password',
+            'first_name': 'bob',
+            'last_name': 'ross'
+        })
+
+        self.assertEqual(Ninja.objects.count(), 1)
