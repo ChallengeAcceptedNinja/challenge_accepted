f5d861fbe32c312d747023cd6d8f832a1fb56a65
Lizzie Gutierrez <elizabeth.wk.gutierrez@gmail.com>
2017-10-08 10:39:23 -0700

    Added validations to Ninja and tested that username and email are unique upon registration.

--- apps/ninjas/models.py
+++ apps/ninjas/models.py
@@ -2,11 +2,40 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, UserManager
+from django.contrib import messages
+
+class NinjaManager(UserManager):
+    def validate_registration(self, data):
+        emails = self.filter(email=data['email'])
+        usernames = self.filter(username=data['username'])
+        errors = {}
+        if len(emails) > 0:
+            errors['email'] = 'Email already exists.'
+        if len(usernames) > 0:
+            errors['username'] = 'Username already exists.'
+        if not errors:
+            new_ninja = self.create_user(
+                data['username'],
+                data['email'],
+                data['password'],
+            )
+            new_ninja.first_name = data['first_name']
+            new_ninja.last_name = data['last_name']
+            new_ninja.save()
+            return new_ninja
+        else:
+            # for tag in errors:
+                # messages.error(request, errors[tag])
+            return None
+            
+
 
 class Ninja(User):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = NinjaManager()
+    
     @property
     def challenges_won(self):
         return 0
--- apps/ninjas/tests.py
+++ apps/ninjas/tests.py
@@ -11,3 +11,43 @@ class UsersTest(TestCase):
         ninjas = Ninja.objects.create_user('bob', 'bobby@ross.com', 'password')
         all_ninjas = Ninja.objects.all()
         self.assertIn(ninjas, all_ninjas)
+
+    def test_that_username_is_unique(self):
+
+        bobby = Ninja.objects.validate_registration({
+            'username': 'bobby',
+            'email': 'bobby@ross.com',
+            'password': 'password',
+            'first_name': 'bob',
+            'last_name': 'ross'
+        })
+
+        jorge = Ninja.objects.validate_registration({
+            'username': 'bobby',
+            'email': 'rossy@ross.com',
+            'password': 'password',
+            'first_name': 'bob',
+            'last_name': 'ross'
+        })
+
+        self.assertEqual(Ninja.objects.count(), 1)
+
+    def test_that_email_is_unique(self):
+    
+        bobby = Ninja.objects.validate_registration({
+            'username': 'bobby',
+            'email': 'bobby@ross.com',
+            'password': 'password',
+            'first_name': 'bob',
+            'last_name': 'ross'
+        })
+
+        jorge = Ninja.objects.validate_registration({
+            'username': 'jorgy',
+            'email': 'bobby@ross.com',
+            'password': 'password',
+            'first_name': 'bob',
+            'last_name': 'ross'
+        })
+
+        self.assertEqual(Ninja.objects.count(), 1)
