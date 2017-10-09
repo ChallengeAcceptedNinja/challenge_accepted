7b0be283d646cd9ca4d7dd2a0a49b61cee180696
Lizzie Gutierrez <elizabeth.wk.gutierrez@gmail.com>
2017-10-08 18:07:18 -0700

    Created ChallengeSession model and related tests.

--- apps/challenge_sessions/models.py
+++ apps/challenge_sessions/models.py
@@ -7,5 +7,31 @@ from ..ninjas.models import Ninja
 class ChallengeSession(models.Model):
     number = models.IntegerField(default=None, null=True)
 
+    @property
+    def winners(self):
+        bouts = self.bouts.all()
+        winners = []
+        for bout in bouts:
+            winners.append(bout.winner)
+        return winners    
 
+    @property
+    def losers(self):
+        bouts = self.bouts.all()
+        losers = []
+        for bout in bouts:
+            losers.append(bout.loser)
+        return losers   
+
+    @property
+    def participants(self):
+        bouts = self.bouts.all()
+        results = []
+        for bout in bouts:
+            participants = bout.participants.all()
+            for participant in participants:
+                results.append(participant)
+        return results    
+        # return self.bouts.prefetch_related('challenge_session').values()
+        # return self.bouts.prefetch_related().iterator()
 
