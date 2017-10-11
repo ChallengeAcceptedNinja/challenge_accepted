b01838617db4dfd1cf6df4417f8bd4c2e93f4515
Lizzie Gutierrez <elizabeth.wk.gutierrez@gmail.com>
2017-10-11 12:50:17 -0700

    Started building templates related to challenges.

--- apps/challenges/views.py
+++ apps/challenges/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.views import View
 
-# Create your views here.
+class ChallengeView(View):
+    def get(self, request):
+        return render(request, 'challenges/signup.html')
+
+    def post(self, request):
+        pass
