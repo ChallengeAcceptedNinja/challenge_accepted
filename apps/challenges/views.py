c219cdbbc641b2cc70221a088c5e19211cf831fb
Cai Lu <30845427+cailinglu@users.noreply.github.com>
2017-10-13 09:13:57 -0700

    Scores are correctly updated. Problem: users not correctly moved forward in the backend

--- apps/challenges/views.py
+++ apps/challenges/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from copy import copy
-import simplejson, datetime
+import simplejson, datetime, math
 
 from .forms import ChallengeInitiateForm
 from .models import Challenge
@@ -64,9 +64,7 @@ class JoinChallenge(View):
         # Join challenge
         try:
             challenge = Challenge.objects.get(id=challenge_id)
-            print "challenge:", challenge
             ninja = Ninja.objects.get(id=self.request.user.id)
-            print "ninja:", ninja
             add_ninja_to_challenge(ninja, challenge)
         except:
             pass
@@ -97,25 +95,78 @@ class DetermineResult(View):
         pass
     
     def post(self, request, bout_id):
-        won = request.POST.get('won')
+        won = request.POST.get('result')
         challenge_id = request.POST.get('challenge_id')
         try:
             ninja = Ninja.objects.get(id=self.request.user.id)
+            # Update this bout
             opponent = None
             bout = SessionBout.objects.get(id=bout_id)
             for participant in bout.participants.all():
                 if participant != ninja:
                     opponent = participant
-            if won:
+            if won == 'True':
                 bout.winner = ninja
                 bout.loser = opponent
+                move_to_next_session(ninja, bout, opponent)
             else:
                 bout.winner = opponent
                 bout.loser = ninja
+                move_to_next_session(opponent, bout, ninja)
             bout.save()
+            # Create a new bout for the next session and add the winner as a participant
         except:
             pass
         return redirect(reverse('challenges:join', kwargs={ 'challenge_id': challenge_id }))
+
+def move_to_next_session(ninja, prev_bout, to_remove=None):
+    # Ninja should be advanced to bout # prev_bout.number / 2 rounded up
+    prev_bout_number = get_bout_number(prev_bout)
+    next_bout_number = int(math.ceil(float(prev_bout_number) / float(2)))
+    # Check if next session exists, if it doesn't create it
+    prev_session = prev_bout.challenge_session
+    prev_session_number = prev_session.number
+    print "prev session number:", prev_session_number
+    challenge = prev_session.challenge
+    print "moving to different session in challenge:", challenge.name
+    try:
+        next_session = ChallengeSession.objects.get(number=prev_session_number + 1)
+    except:
+        next_session = ChallengeSession.objects.create(number=prev_session_number + 1, challenge=challenge)
+    next_session.save()
+    print "next session number:", next_session.number
+    # If bout of next_bout_number doesn't exist, create it
+    try:
+        next_bout = get_bout_from_session(next_bout_number, next_session)
+    except:
+        next_bout = SessionBout.objects.create(challenge_session=next_session)
+    # Add ninja to this bout
+    print "next bout participants:", next_bout.participants.all()
+    next_bout.participants.add(ninja)
+    next_bout.save()
+    if to_remove:
+        next_bout.participants.remove(to_remove)
+    print next_bout.participants.all()
+    print "moved", ninja.username, "to the next session and bout"
+
+def get_bout_number(target_bout):
+    count = 0
+    all_bouts = target_bout.challenge_session.bouts.all().order_by('id')
+    for bout in all_bouts:
+        count += 1
+        if bout == target_bout:
+            return count
+    raise ValueError('could not find that bout')
+
+def get_bout_from_session(bout_number, session):
+    count = 0
+    all_bouts = session.bouts.all().order_by('id')
+    for bout in all_bouts:
+        count += 1
+        if count == bout_number:
+            print "next bout participants:", bout.participants.all()
+            return bout
+    raise ValueError('could not find that bout in that session')
 
 def ninja_in(participants, ninja):
     for participant in participants:
@@ -139,42 +190,56 @@ def generate_challenge_data(challenge_id):
         challenge = Challenge.objects.get(id=challenge_id)
         sessions = ChallengeSession.objects.filter(challenge=challenge).order_by('number')
         data = {
-            'teams': [],
-            'results': []
+            'teams': None,
+            'results': None
         }
     except:
         data = {}
     
-    # Format session bouts data
-    # TODO: FIX ME
     try:
-        for session in sessions.all():
+        teams = []
+        for session in sessions.all().order_by('number'):
             session_results = []
-            for bout in session.bouts.all():
+            for bout in session.bouts.all().order_by('id'):
                 participants = []
-                bout_scores = []
-                for participant in bout.participants.all():
+                bout_scores = [None, None]
+                for participant in bout.participants.all().order_by('id'):
                     participants.append(participant.username)
+                    score = None
                     if bout.winner == participant:
-                        bout_scores.append(1)
+                        score = 1
                     elif bout.loser == participant:
-                        bout_scores.append(0)
+                        score = 0
+ 
+                    if session.number == 1:
+                        ninja_index = len(participants) - 1
                     else:
-                        bout_scores.append(None)
-                if len(bout_scores) < 2:
-                    bout_scores.append(None)
+                        locations = get_ninja_position_in_team(teams, participant)
+                        ninja_index = locations['ninja_index']
+                        # bout_scores[locations['ninja_index']] = score
+                    bout_scores[ninja_index] = score
+
                 session_results.append(bout_scores)
+                
                 if session.number == 1:
-                    if not bout.is_full:
+                    while len(participants) < 2:
                         participants.append(None)
-                        bout_scores.append(None)
-                    if len(participants) < 2:
-                        participants.append(None)
-                    data['teams'].append(participants)
-            data['results'].append(session_results)
+                    teams.append(participants)
+            data['teams'] = teams
+            data['results'] = session_results
     except:
         pass
     return data
+
+def get_ninja_position_in_team(teams, ninja):
+    for team_number in range(0, len(teams)):
+        for ninja_index in range(0, len(team)):
+            if team[ninja_index] == ninja.username:
+                return {
+                    'team_number': team_number,
+                    'ninja_index': ninja_index
+                }
+    raise ValueError('could not find that ninja in the teams provided')
 
 def add_ninja_to_challenge(ninja, challenge):
     # Ninja can only join the first session
@@ -182,15 +247,31 @@ def add_ninja_to_challenge(ninja, challenge):
         first_session = ChallengeSession.objects.get(challenge=challenge, number=1)
         all_bouts = SessionBout.objects.filter(challenge_session=first_session)
         for bout in all_bouts:
+            # If not full, add ninja
             if not bout.is_full:
+                opponent = bout.participants.first()
                 bout.participants.add(ninja)
                 bout.save()
+                # check if opponent needs to be removed from next session's participants
+                remove_from_next_bout(bout, opponent)
+                # If still not full (ninja is the only participant), move ninja onwards
+                if not bout.is_full:
+                    print "moving", ninja.username, "to next session because empty"
+                    move_to_next_session(ninja, bout)
                 break
         next_open_bout = generate_bouts()
         next_open_bout.participants.add(ninja)
         next_open_bout.save()
+        if not next_bout_number.is_full:
+            print "moving", ninja.username, "to next session because empty"
+            move_to_next_session(ninja, bout)
     except:
         pass
+
+def remove_from_next_bout(bout, opponent):
+    prev_bout_number = get_bout_number(bout)
+    next_bout_number = int(math.ceil(float(prev_bout_number) / float(2)))
+    next_open_bout.participants.remove(opponent)
 
 def bout_is_active(bout):
     now = datetime.date.today()
